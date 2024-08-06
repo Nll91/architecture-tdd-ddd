0c0b2d61324d505012bc90c0fa9fad00379e3c0d
norman-luengo <norman.luengo@sdggroup.com>
2024-08-06 20:34:38 +0200

    feat: add logic to Batch class

--- models/model.py
+++ models/model.py
@@ -1,18 +1,66 @@
 from dataclasses import dataclass
+from typing import Optional
+from datetime import date
 
 
-@dataclass
+@dataclass(frozen=True)
 class OrderedLine:
-    id: str
+    order_id: str
     sku: str
+    quantity: int
 
 
 class Batch:
     """
     Model domain
     """
-    def __init__(self):
-        pass
 
-    def associate(self):
-        pass
+    def __init__(
+            self,
+            reference: str,
+            sku: str,
+            qty: int,
+            eta: Optional[date]
+    ):
+        self.reference = reference
+        self.sku = sku
+        self._purchased_qty = qty
+        self.eta = eta
+        self._allocations = set()
+
+    def allocate(self, order: OrderedLine):
+        if self.can_allocate(order):
+            self._allocations.add(order)
+
+    def deallocate(self, order: OrderedLine):
+        if order in self._allocations:
+            self._allocations.remove(order)
+
+    @property
+    def allocated_quantity(self) -> int:
+        return sum(order.quantity for order in self._allocations)
+
+    @property
+    def available_quantity(self) -> int:
+        return self._purchased_qty - self.allocated_quantity
+
+    def can_allocate(self, order: OrderedLine) -> bool:
+        return order.quantity <= self.available_quantity and order.sku == self.sku
+
+    def __eq__(self, other):
+        if not isinstance(other, Batch):
+            return False
+        return other.reference == self.reference
+
+    def __hash__(self):
+        return hash(self.reference)
+
+    def __gt__(self, other):
+        if self.eta is None:
+            return False
+        if other.eta is None:
+            return True
+        return self.eta > other.eta
+
+
+
--- tests/test_model.py
+++ tests/test_model.py
@@ -1,5 +1,52 @@
+import pytest
+from datetime import date
 from models.model import Batch, OrderedLine
 
 
+def test_order_equality():
+    first_order = OrderedLine(order_id='1234', sku='chair', quantity=3)
+    second_order = OrderedLine(order_id='1234', sku='chair', quantity=3)
+    assert first_order == second_order
 
+
+def test_order_inequality():
+    first_order = OrderedLine(order_id='1234', sku='chair', quantity=3)
+    second_order = OrderedLine(order_id='124', sku='chair', quantity=3)
+    assert first_order != second_order
+
+
+def get_batch_and_order_line(sku, batch_qty, line_qty):
+    return (
+        Batch(reference='Batch-001', sku=sku, qty=batch_qty, eta=date.today()),
+        OrderedLine(order_id='1234', sku=sku, quantity=line_qty)
+    )
+
+
+def test_order_line_can_be_allocated():
+    batch_obj, order_obj = get_batch_and_order_line('123', 10, 2)
+    batch_obj.allocate(order_obj)
+    expected_quantity = 8
+    assert batch_obj.available_quantity == expected_quantity
+
+
+def test_out_of_stock_case():
+    batch_obj, order_obj = get_batch_and_order_line('123', 10, 20)
+    assert batch_obj.can_allocate(order_obj) is False
+
+
+def test_sku_equality():
+    order_obj = OrderedLine(order_id='1234', sku='chair', quantity=3)
+    batch_obj = Batch(reference='Batch-001', sku='table', qty=3, eta=date.today())
+    assert batch_obj.can_allocate(order_obj) is False
+
+
+def test_batch_equality():
+    batch1 = Batch(reference='Batch-001', sku='table', qty=3, eta=date.today())
+    batch2 = Batch(reference='Batch-001', sku='chair', qty=3, eta=date.today())
+    assert batch1 == batch2
+
+def test_batch_inequality():
+    batch1 = Batch(reference='Batch-001', sku='table', qty=3, eta=date.today())
+    batch2 = Batch(reference='Batch-002', sku='chair', qty=3, eta=date.today())
+    assert batch1 != batch2
 
